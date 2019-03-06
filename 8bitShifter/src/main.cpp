b3dd8d3fa8f437f240f007f35fd15c85c07d1220
Eduardo Maestri Righes <eduardo@emr-shiny.local>
2019-03-06 12:17:25 +0000

    8-bit shifter example.

---
+++ 8bitShifter/src/main.cpp
@@ -0,0 +1,28 @@
+
+#include <Arduino.h>
+
+const int latchPin = 8;
+const int clockPin = 12;
+const int dataPin = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);
+    digitalWrite(latchPin, HIGH);
+    delay(200);
+
+    Serial.print("value: ");
+    Serial.println(numberToDisplay);
+  }
+}
